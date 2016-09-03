a4a0e50b7f91d988e48f013b7c4dcfe8ea52b9aa
RICARDO <RICARDO@Rick>
2016-09-03 00:19:54 -0500

    ACA MODIFIQUE EL INDEX

--- views/site/index.php
+++ views/site/index.php
@@ -24,6 +24,7 @@ $this->title = 'My Yii Application';
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
+                <p>HOLA LALA</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
